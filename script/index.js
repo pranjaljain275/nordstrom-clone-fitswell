6d8923872c927a73b24cecfd110cf9830f08f5b4
Pranjal Jain <pranjaljain275@gmail.com>
2022-11-14 00:38:54 +0530

    construct week project web101

--- script/index.js
+++ script/index.js
@@ -1,14 +1,28 @@
 // dropdown
 let toggle = document.querySelectorAll(".toggle");
 
-for(let i=0; i<toggle.length; i++) {
-  toggle[i].addEventListener("click", ()=> {
+for (let i = 0; i < toggle.length; i++) {
+  toggle[i].addEventListener("click", () => {
     toggle[i].classList.toggle("active");
     let main = toggle[i].nextElementSibling;
-    if(main.style.display == "flex") {
+    if (main.style.display == "flex") {
       main.style.display = "none";
     } else {
       main.style.display = "flex";
     }
-  })
+  });
 }
+
+// signup dropdown
+let signdrop = document.querySelector("#logged");
+console.log(signdrop);
+
+signdrop.addEventListener("click", () => {
+  signdrop.classList.toggle("active2");
+  let main = signdrop.nextElementSibling;
+  if (main.style.display == "flex") {
+    main.style.display = "none";
+  } else {
+    main.style.display = "flex";
+  }
+});
